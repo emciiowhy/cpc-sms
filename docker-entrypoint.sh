6550ee99215ada6a330dbc78c68f1a09f1376fc1
emciiowhy <mcmcyap07@gmail.com>
2026-04-10 14:31:46 +0800

    Update entrypoint to seed database

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-# Clear old settings
-php artisan config:clear
-php artisan cache:clear
+# Optimize the application for production
+# This combines config:cache, route:cache, and view:cache
+php artisan optimize
 
-# Run migrations
-php artisan migrate --force
+# Run migrations and seed the database with your test accounts
+# The --force flag is required to run migrations in production
+php artisan migrate --force --seed
 
 # Start Apache in the foreground
 exec apache2-foreground
